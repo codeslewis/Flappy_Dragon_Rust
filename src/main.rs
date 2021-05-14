7e40dbe1c8d549b062c8804611f0cf5bb4c32c63
codeslewis <lewis.sneddon@icloud.com>
2021-05-14 14:47:43 +0100

    added player to state

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ enum GameMode {
 }
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0
             mode: GameMode::Menu,
         }
     }
